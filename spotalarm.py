cf0cb0890ff5fc40f436f0ebd95275231d1aea7a
Albin <info@albinso.com>
2016-10-11 12:55:50 +0000

    Spotalarm is broken

--- spotalarm.py
+++ spotalarm.py
@@ -9,7 +9,6 @@ def act():
     call(["ncmpcpp", "play"])
 
 def wait_start(runTime, action):
-    sleep(60*60*1)
     startTime = time(*(map(int, runTime.split(':'))))
     print(startTime)
     while startTime > datetime.today().time(): # you can add here any additional variable to break loop if necessary
