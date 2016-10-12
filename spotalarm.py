812293cf6c4705bb22dc03d3d8dd12ae415170f3
albinso <albinso@kth.se>
2016-10-12 15:29:40 +0200

    Small bug that stopped alarm script

--- spotalarm.py
+++ spotalarm.py
@@ -13,7 +13,7 @@ def wait_start(runTime, action):
     startTime = time(*(map(int, runTime.split(':'))))
     while startTime < now:
     	sleep(60)
-    	now = datetime.today.time()
+    	now = datetime.today().time()
 
     while startTime > datetime.today().time(): # you can add here any additional variable to break loop if necessary
         sleep(60) # you can change 1 sec interval to any other
