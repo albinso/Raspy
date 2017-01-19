214e936353aa154784c448a9dd6424d70f5fb3d7
albinso <albinso@kth.se>
2017-01-19 03:13:36 +0100

    Changed spaces to tabs

--- spotalarm.py
+++ spotalarm.py
@@ -5,22 +5,22 @@ import sys
 
 def act():
 	call(["mpc", "volume", "0"])
-    call(["ncmpcpp", "-h", "192.168.0.100", "prev"])
-    call(["ncmpcpp", "play"])
-    
-    for i in range(100):
-    	call(["mpc", "volume", "+1"])
-    	sleep(1)
+	call(["ncmpcpp", "-h", "192.168.0.100", "prev"])
+	call(["ncmpcpp", "play"])
+	
+	for i in range(100):
+		call(["mpc", "volume", "+1"])
+		sleep(1)
 
 def wait_start(runTime, action):
-    now = datetime.today().time()
-    startTime = time(*(map(int, runTime.split(':'))))
-    while startTime < now:
-    	sleep(60)
-    	now = datetime.today().time()
+	now = datetime.today().time()
+	startTime = time(*(map(int, runTime.split(':'))))
+	while startTime < now:
+		sleep(60)
+		now = datetime.today().time()
 
-    while startTime > datetime.today().time(): # you can add here any additional variable to break loop if necessary
-        sleep(60) # you can change 1 sec interval to any other
-    return act()
+	while startTime > datetime.today().time(): # you can add here any additional variable to break loop if necessary
+		sleep(60) # you can change 1 sec interval to any other
+	return act()
 
 wait_start(sys.argv[1], act)
