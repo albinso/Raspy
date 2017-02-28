59e0f7554217124ece073e1aac395b24305987b3
Albin <info@albinso.com>
2017-02-28 02:06:15 +0100

    Initial volume and playlist works

--- raspy/__init__.py
+++ raspy/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from raspy.models.AlarmHandler import AlarmHandler
 from raspy.models.api_gen import AlarmApiGenerator
 from raspy.models.MpdController import MpdController
+import time
 
 START_VOLUME = 15
 
@@ -9,7 +10,7 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 app.url_map.strict_slashes = False
 
 alarm_handler = AlarmHandler()
-mpd_controller = MpdController()
+mpd_controller = MpdController(default_volume=START_VOLUME)
 mpd_controller.set_volume(START_VOLUME)
 api_gen = AlarmApiGenerator(alarm_handler, mpd_controller)
 
