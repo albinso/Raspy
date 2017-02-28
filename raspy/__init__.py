0b6ea8f2d7914d71645133b57d4ec7d8027deb29
albinso <albinso@kth.se>
2017-02-28 01:45:58 +0100

    Made default volume an option

--- raspy/__init__.py
+++ raspy/__init__.py
@@ -3,10 +3,13 @@ from raspy.models.AlarmHandler import AlarmHandler
 from raspy.models.api_gen import AlarmApiGenerator
 from raspy.models.MpdController import MpdController
 
+START_VOLUME = 15
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.url_map.strict_slashes = False
 
 alarm_handler = AlarmHandler()
 mpd_controller = MpdController()
+mpd_controller.set_volume(START_VOLUME)
 api_gen = AlarmApiGenerator(alarm_handler, mpd_controller)
 
