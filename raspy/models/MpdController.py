48d349224817c4fe4f7509f467d10cd498fcccfa
albinso <albinso@kth.se>
2017-02-28 02:02:08 +0100

    Waits until mopidy accepts request to hand back control to raspy

--- raspy/models/MpdController.py
+++ raspy/models/MpdController.py
@@ -1,13 +1,18 @@
 from subprocess import call, Popen, STDOUT
 import os
+import time
 
 class MpdController:
 
-	def __init__(self):
+	def __init__(self, default_volume=100):
 		FNULL = open(os.devnull, 'w')
 		self.process = Popen(["mopidy"], stdout=FNULL, stderr=STDOUT)
+		code = 1
+		while code != 0:
+			code = self.set_volume(default_volume)
+			time.sleep(1)
 		call(['mpc', '-p', '6680', 'load', 'Cheese it (by heerkip)'])
-		self.vol = 100
+		self.vol = default_volume
 
 	def set_volume(self, vol):
 		self.vol = vol
